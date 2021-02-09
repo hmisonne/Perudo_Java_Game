3372d85b9780a365cb6fabc39b8756aa5cc92711
hmisonne <helene.misonne@gmail.com>
2021-02-08 16:14:15 -0800

    refactor: Created initializeGame and getUserSelection methods and added robot selection

--- src/com/example/mypackage/Main.java
+++ src/com/example/mypackage/Main.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 public class Main {
     public static String[] robotNames = {"Yellow", "Blue", "Pink", "Blue","Green"};
+    public static boolean quit = false;
 
     public static void main(String[] args) {
         try{
@@ -45,6 +46,53 @@ public class Main {
     }
     public static void play() {
 //        Initialize Game
+        Perudo perudo = initializeGame();
+        while (perudo.isRunning() && !quit){
+            if (perudo.isfirstRound()){
+                perudo.shuffleDice();
+                System.out.println("It's "+perudo.getCurrentPlayer().getName() +" turn");
+                if (perudo.getCurrentPlayer() instanceof RobotPlayer){
+                    RobotPlayer robotPlayer = (RobotPlayer) perudo.getCurrentPlayer();
+                    int[] newBet = robotPlayer.makeABet(perudo.getNumberOfDice());
+                    perudo.makeABet(newBet);
+                }
+                else{
+                    System.out.println("You currently have: "+ Arrays.toString(perudo.getCurrentPlayer().getDiceValues()));
+                    System.out.println("Choose\n" +
+                            "1 to make a bet\n" +
+                            "0 to quit");
+                    getUserSelection(perudo);
+                }
+
+            }
+            else {
+                if (perudo.currentBetIsCorrect()){
+                    perudo.nextPlayer();
+                }
+                System.out.println("\n################################\n");
+                System.out.println("It's "+perudo.getCurrentPlayer().getName() +"'s turn,");
+                if (perudo.getCurrentPlayer() instanceof RobotPlayer){
+                    RobotPlayer robotPlayer = (RobotPlayer) perudo.getCurrentPlayer();
+                    int[] newBet = robotPlayer.makeABet(perudo.getCurrentBet());
+                    perudo.makeABet(newBet);
+                }
+                else {
+                    System.out.println("You currently have: "+ Arrays.toString(perudo.getCurrentPlayer().getDiceValues()));
+                    System.out.println("Choose:\n" +
+                            "\t1 to make a bet\n" +
+                            "\t2 to reveal the dice\n" +
+                            "\t0 to quit");
+                    getUserSelection(perudo);
+                }
+
+            }
+
+
+
+        }
+    }
+
+    public static Perudo initializeGame(){
         Perudo perudo = new Perudo();
         Scanner scanner = new Scanner(System.in);
 //        Add players
@@ -62,46 +110,28 @@ public class Main {
         }
 //        Set current player
         perudo.setCurrentPlayer(new Random().nextInt(playerNumber));
+        return perudo;
+    }
 
-        boolean quit = false;
-        while (perudo.isRunning() && !quit){
-            if (perudo.isfirstRound()){
-                perudo.shuffleDice();
-                System.out.println("It's "+perudo.getCurrentPlayer().getName() +" turn");
-                System.out.println("Choose\n" +
-                        "1 to make a bet\n" +
-                        "0 to quit");
-            }
-            else {
-                if (perudo.currentBetIsCorrect()){
-                    perudo.nextPlayer();
-                }
-                System.out.println("\n################################\n");
-                System.out.println("It's "+perudo.getCurrentPlayer().getName() +"'s turn,");
-                System.out.println("Choose:\n" +
-                        "\t1 to make a bet\n" +
-                        "\t2 to reveal the dice\n" +
-                        "\t0 to quit");
-            }
-            int choice = scanner.nextInt();
-            scanner.nextLine();
-            switch (choice) {
-                case 0:
-                    quit = true;
-                    break;
-                case 1:
-                    System.out.print("Enter your bet for number of dice: ");
-                    int numOfDice = getInt();
-                    System.out.print("Enter your bet for value of dice: ");
-                    int dieValue = getInt(1,6);
-                    perudo.makeABet(new int[]{numOfDice, dieValue});
-                    break;
-                case 2:
-                    perudo.revealDice();
-                    break;
-            }
+    public static void getUserSelection(Perudo perudo){
 
-
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+        scanner.nextLine();
+        switch (choice) {
+            case 0:
+                quit = true;
+                break;
+            case 1:
+                System.out.print("Enter your bet for number of dice: ");
+                int numOfDice = getInt();
+                System.out.print("Enter your bet for value of dice: ");
+                int dieValue = getInt(1,6);
+                perudo.makeABet(new int[]{numOfDice, dieValue});
+                break;
+            case 2:
+                perudo.revealDice();
+                break;
         }
     }
 }
