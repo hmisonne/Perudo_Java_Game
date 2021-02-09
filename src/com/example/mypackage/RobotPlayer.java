2a6f08bf716d04e6e35e7d08b10a6054777f5d92
hmisonne <helene.misonne@gmail.com>
2021-02-09 14:13:02 -0800

    refactor: Updated makeABet function to use a random number for dieValue and calculate numberOfDice accordingly

--- src/com/example/mypackage/RobotPlayer.java
+++ src/com/example/mypackage/RobotPlayer.java
@@ -7,11 +7,41 @@ public class RobotPlayer extends Player{
         super(name);
     }
 
-    public int[] makeABet(int[] previousBet){
-        return new int[] {previousBet[0]+1,previousBet[1]};
+    public int[] makeABet(int[] currentBet){
+//        Choose a random number for dieValue and calculate numberOfDice accordingly
+        int dieValue = new Random().nextInt(6)+1;
+        int numberOfDice = currentBet[0];
+//        First option: new Bet dieValue == currentBet die Value
+//          Includes also new bet with a lower die value than current bet -> Increase quantity of die by 1.
+        if(dieValue == currentBet[1] || (dieValue > 1 && dieValue < currentBet[1])){
+            numberOfDice ++ ;
+        }
+//        Other options: new Bet dieValue != currentBet die Value
+//          If current bet is with Pacos, double the quantity of dice
+        else if(currentBet[1]==1){
+            numberOfDice *= 2;
+            numberOfDice ++;
+        }
+//          If new Bet is with Pacos, divide by 2 the quantity of dice
+        else if(dieValue == 1){
+            numberOfDice /= 2;
+            if (currentBet[0] % 2 != 0){
+                numberOfDice ++;
+            }
+        }
+//          Else, new die value is higher than current die value, keep die quantity
+
+        return new int[] {numberOfDice,dieValue};
+
     }
     public int[] makeABet(int numberOfDice){
-        return new int[] {numberOfDice/3-1, new Random().nextInt(6)+2};
+//            The die's value will be between 2 and 6 (random number between 0 and 4, +2)
+        int dieValue = new Random().nextInt(5)+2;
+        if (numberOfDice > 3){
+            return new int[] {(numberOfDice/3)-1, dieValue};
+        } else {
+            return new int[] {1, dieValue};
+        }
     }
     public boolean decideToBet(int[] currentBet, int numberOfDice){
         int currNumOfDice = currentBet[0];
