d6f16747ef7bd15250eaba2e4076817e67a097f8
hmisonne <helene.misonne@gmail.com>
2021-02-08 16:12:03 -0800

    feat: Overloaded makeABet method for first round (no previous bet)

--- src/com/example/mypackage/RobotPlayer.java
+++ src/com/example/mypackage/RobotPlayer.java
@@ -1,5 +1,7 @@
 package com.example.mypackage;
 
+import java.util.Random;
+
 public class RobotPlayer extends Player{
     public RobotPlayer(String name) {
         super(name);
@@ -7,6 +9,9 @@ public class RobotPlayer extends Player{
 
     public int[] makeABet(int[] previousBet){
         return new int[] {previousBet[0]+1,previousBet[1]};
+    }
+    public int[] makeABet(int numberOfDice){
+        return new int[] {numberOfDice/3-1, new Random().nextInt(6)+1};
     }
     public boolean decideToBet(int[] currentBet, int numberOfDice){
         int currNumOfDice = currentBet[0];
